8c68a2452fa7c7a558e1684f84a2755452126b32
Louis <98012626+louismrn@users.noreply.github.com>
2023-08-01 17:33:23 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//  https://dev.twitch.tv/docs/embed
+var embed = new Twitch.Embed("twitch-embed", {
+  //muted: true,
+  //autoplay: false,
+  theme: "dark",
+  width: "100%",
+  height: "100%",
+  channel: "eotones"
+});
